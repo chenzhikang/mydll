160cc6cfc0020b21643304ba17e81a4fa4511229
陈志康 <chenzhikang0817@outlook.com>
2016-08-07 17:42:30 +0800

    更改.net framework版本4.5

---
+++ CallTestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mydll;
+
+namespace CallTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePicturePath = @"C:\Users\ljj\Desktop\20160805_035209_584.jpg";
+            string targetPath = PictureHelper.CreateSpecSizePic(sourcePicturePath, 400, 0);
+
+            Console.WriteLine(targetPath);
+            Console.Read();
+        }
+    }
+}
